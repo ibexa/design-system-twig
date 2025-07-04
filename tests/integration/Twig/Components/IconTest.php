a2ef8585f557fd8689a1739690f883ea16e214b2
Adam Wójs <adam@wojs.pl>
2025-07-04 09:40:39 +0200

    Added Icon and CustomIcon components (tests)

---
+++ tests/integration/Twig/Components/IconTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\Integration\DesignSystemTwig\Twig\Components;
+
+use Ibexa\DesignSystemTwig\Twig\Components\Icon;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\UX\TwigComponent\Test\InteractsWithTwigComponents;
+
+final class IconTest extends KernelTestCase
+{
+    use InteractsWithTwigComponents;
+
+    public function testMount(): void
+    {
+        $component = $this->mountTwigComponent(
+            'ibexa:icon',
+            [
+                'name' => 'trash',
+            ]
+        );
+
+        self::assertInstanceOf(Icon::class, $component);
+        self::assertSame('trash', $component->name);
+    }
+
+    public function testDefaultRender(): void
+    {
+        $rendered = $this->renderTwigComponent(
+            'ibexa:icon',
+            [
+                'name' => 'trash',
+            ]
+        );
+
+        self::assertStringEndsWith('#trash', $rendered->crawler()->filter('svg use')->first()->attr('xlink:href'));
+    }
+}
