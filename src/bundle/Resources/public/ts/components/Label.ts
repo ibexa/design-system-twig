f951b813e60c12f0c22a00009479234d74855fbf
Michał Grabowski <michal.grabowski@ibexa.co>
2025-08-19 15:51:33 +0200

    IBX-10509: Label (#12)

---
+++ src/bundle/Resources/public/ts/components/Label.ts
@@ -0,0 +1,16 @@
+import Base from '../shared/Base';
+
+export default class Label extends Base {
+    private _error = false;
+
+    set error(value: boolean) {
+        this._error = value;
+        this.container.classList.toggle('ids-label--error', value);
+    }
+
+    get error(): boolean {
+        return this._error;
+    }
+}
+
+export type LabelType = InstanceType<typeof Label>;
