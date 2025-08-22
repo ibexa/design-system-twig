8738a1008dd7054454c9336f9dfb6cc157374b34
Michał Grabowski <michal.grabowski@ibexa.co>
2025-08-22 15:54:12 +0200

    IBX-7999: IBX-7999: Checkbox and ThreeStateCheckbox

--- src/bundle/Resources/public/ts/components/inputs/Checkbox.ts
+++ src/bundle/Resources/public/ts/components/inputs/Checkbox.ts
@@ -1,17 +1,3 @@
-import Base from '../../shared/Base';
+import BaseCheckbox from '../../shared/BaseCheckbox';
 
-export default class Checkbox extends Base {
-    protected _inputElement: HTMLInputElement;
-
-    constructor(container: HTMLDivElement) {
-        super(container);
-
-        const inputElement = container.querySelector<HTMLInputElement>('.ids-input');
-
-        if (!inputElement) {
-            throw new Error('Checkbox: Required elements are missing in the container.');
-        }
-
-        this._inputElement = inputElement;
-    }
-}
+export default class Checkbox extends BaseCheckbox {}
