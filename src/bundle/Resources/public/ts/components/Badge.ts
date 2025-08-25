9fdc87eb34da213a1ff741e5ed7399d8a76b2147
Aleksandra Bozek <aleksandra.m.bozek@gmail.com>
2025-08-25 13:08:27 +0200

    IBX-7880: Badge

---
+++ src/bundle/Resources/public/ts/components/Badge.ts
@@ -0,0 +1,3 @@
+import BaseBadge from '../shared/BaseBadge';
+
+export default class Badge extends BaseBadge {}
---
+++ src/lib/Twig/Components/Badge.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\DesignSystemTwig\Twig\Components;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use Symfony\UX\TwigComponent\Attribute\ExposeInTemplate;
+use Symfony\UX\TwigComponent\Attribute\PreMount;
+
+#[AsTwigComponent]
+final class Badge
+{
+    private const MAX_BADGE_VALUE = 99;
+
+    public string $size = 'medium';
+    public int $value = 1;
+    /**
+     * @param array<string, mixed> $props
+     */
+    #[PreMount]
+    public function validate(array $props): array
+    {
+        $resolver = new OptionsResolver();
+        $resolver->setIgnoreUndefined(true);
+        $resolver
+            ->define('size')
+            ->allowedValues('small', 'medium')
+            ->default('medium');
+        $resolver
+            ->define('value')
+            ->allowedTypes('int')
+            ->required();
+
+        return $resolver->resolve($props);
+    }
+
+    #[ExposeInTemplate('is_expanded')]
+    public function isExpanded(): bool
+    {
+        return $this->value > self::MAX_BADGE_VALUE;
+    }
+
+    #[ExposeInTemplate('formatted_value')]
+    public function getFormattedValue(): string
+    {
+        if ($this->value > self::MAX_BADGE_VALUE) {
+            return '99+';
+        }
+        
+        return (string)$this->value;
+    }
+}
