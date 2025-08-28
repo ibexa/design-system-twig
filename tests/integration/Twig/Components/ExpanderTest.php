0048808c689eb4182b11362e38470b1469083e67
Mikolaj Adamczyk <mikolaj.adamczyk@ez.no>
2025-08-28 09:10:19 +0200

    IBX-10538: Added tests for twig components (#17)

---
+++ tests/integration/Twig/Components/ExpanderTest.php
@@ -0,0 +1,196 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\Integration\DesignSystemTwig\Twig\Components;
+
+use Ibexa\DesignSystemTwig\Twig\Components\Expander;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\UX\TwigComponent\Test\InteractsWithTwigComponents;
+
+final class ExpanderTest extends KernelTestCase
+{
+    use InteractsWithTwigComponents;
+
+    public function testMount(): void
+    {
+        $component = $this->mountTwigComponent(
+            'ibexa:Expander',
+            [
+                'type' => 'caret',
+                'is_expanded' => true,
+                'expand_label' => 'Show',
+                'collapse_label' => 'Hide',
+                'has_icon' => true,
+                'message' => 'Toggle section',
+            ]
+        );
+
+        self::assertInstanceOf(Expander::class, $component, 'Component should be instance of Expander');
+        self::assertSame('caret', $component->type, 'Type should be "caret"');
+        self::assertTrue($component->is_expanded, 'is_expanded should be true');
+        self::assertSame('Show', $component->expand_label, 'Expand label should be passed through');
+        self::assertSame('Hide', $component->collapse_label, 'Collapse label should be passed through');
+        self::assertTrue($component->has_icon, 'has_icon should be true');
+        self::assertSame('arrow-caret-down', $component->iconName(), 'caret type should map to arrow-caret-down');
+    }
+
+    public function testRenderExpandedState(): void
+    {
+        $rendered = $this->renderTwigComponent(
+            'ibexa:Expander',
+            [
+                'type' => 'caret',
+                'is_expanded' => true,
+                'expand_label' => 'Show',
+                'collapse_label' => 'Hide',
+                'has_icon' => true,
+                'message' => 'Click to hide section',
+            ]
+        );
+
+        $crawler = $rendered->crawler();
+        $button = $this->getButton($crawler);
+
+        self::assertSame(1, $button->count(), 'There should be exactly one expander button');
+        self::assertSame('button', $button->attr('type'), 'Expander should render as <button type="button">');
+
+        $classAttr = (string) $button->attr('class');
+        self::assertStringContainsString('ids-expander', $classAttr, 'Base expander class is missing');
+        self::assertStringContainsString('ids-expander--has-label', $classAttr, 'has-label class is missing');
+        self::assertStringContainsString('ids-expander--has-icon', $classAttr, 'has-icon class is missing');
+        self::assertStringContainsString('ids-expander--is-expanded', $classAttr, 'is-expanded class is missing');
+
+        self::assertSame('true', $button->attr('aria-expanded'), 'aria-expanded should be "true" in expanded state');
+        self::assertSame('Hide', trim($button->filter('.ids-expander__label')->text()), 'Label should match collapse_label');
+
+        $use = $button->filter('svg use')->first();
+        self::assertSame(1, $use->count(), 'Icon <use> element should be present');
+        $href = $use->attr('xlink:href');
+        self::assertNotEmpty($href, 'Icon href should not be empty');
+        self::assertStringEndsWith('#arrow-caret-down', $href, 'Caret type should map to arrow-caret-down');
+    }
+
+    public function testRenderCollapsedState(): void
+    {
+        $rendered = $this->renderTwigComponent(
+            'ibexa:Expander',
+            [
+                'type' => 'caret',
+                'is_expanded' => false,
+                'expand_label' => 'Show',
+                'collapse_label' => 'Hide',
+                'has_icon' => true,
+                'message' => 'Click to expand',
+            ]
+        );
+
+        $crawler = $rendered->crawler();
+        $button = $this->getButton($crawler);
+
+        $classAttr = (string) $button->attr('class');
+        self::assertStringContainsString('ids-expander', $classAttr, 'Base class should be present');
+        self::assertStringNotContainsString('ids-expander--is-expanded', $classAttr, 'is-expanded class should not be present');
+        self::assertSame('false', $button->attr('aria-expanded'), 'aria-expanded should be "false" in collapsed state');
+        self::assertSame('Show', trim($button->filter('.ids-expander__label')->text()), 'Label should match expand_label');
+    }
+
+    public function testChevronVariantRendersChevronIcon(): void
+    {
+        $rendered = $this->renderTwigComponent(
+            'ibexa:Expander',
+            [
+                'type' => 'chevron',
+                'is_expanded' => false,
+                'expand_label' => 'More',
+                'collapse_label' => 'Less',
+                'has_icon' => true,
+                'message' => 'Toggle more info',
+            ]
+        );
+
+        $crawler = $rendered->crawler();
+        $use = $this->getButton($crawler)->filter('svg use')->first();
+
+        self::assertSame(1, $use->count(), 'Icon should be rendered');
+        $href = $use->attr('xlink:href');
+        self::assertNotEmpty($href, 'Icon href should not be empty');
+        self::assertStringEndsWith('#arrow-chevron-down', $href, 'Chevron type should map to arrow-chevron-down');
+    }
+
+    public function testNoIconWhenHasIconIsFalse(): void
+    {
+        $rendered = $this->renderTwigComponent(
+            'ibexa:Expander',
+            [
+                'type' => 'caret',
+                'is_expanded' => false,
+                'expand_label' => 'Show',
+                'collapse_label' => 'Hide',
+                'has_icon' => false,
+                'message' => 'Just text',
+            ]
+        );
+
+        $crawler = $rendered->crawler();
+        $button = $this->getButton($crawler);
+
+        $classAttr = (string) $button->attr('class');
+        self::assertStringNotContainsString('ids-expander--has-icon', $classAttr, 'has-icon class should not be present');
+        self::assertSame(0, $button->filter('svg use')->count(), 'Icon should not be rendered');
+    }
+
+    public function testDataAttributesReflectLabels(): void
+    {
+        $rendered = $this->renderTwigComponent(
+            'ibexa:Expander',
+            [
+                'type' => 'caret',
+                'is_expanded' => true,
+                'expand_label' => 'Open more',
+                'collapse_label' => 'Close',
+                'has_icon' => true,
+                'message' => 'open-close',
+            ]
+        );
+
+        $button = $this->getButton($rendered->crawler());
+        self::assertSame('Open more', $button->attr('data-expand-label'), 'data-expand-label should match');
+        self::assertSame('Close', $button->attr('data-collapse-label'), 'data-collapse-label should match');
+    }
+
+    public function testMergesCustomClassesFromAttributesIntoButton(): void
+    {
+        $rendered = $this->renderTwigComponent(
+            'ibexa:Expander',
+            [
+                'type' => 'caret',
+                'is_expanded' => false,
+                'expand_label' => 'Show',
+                'collapse_label' => 'Hide',
+                'has_icon' => false,
+                'message' => 'msg',
+                'attributes' => [
+                    'class' => 'u-mt-2 custom-hook',
+                ],
+            ]
+        );
+
+        $button = $this->getButton($rendered->crawler());
+        $classAttr = (string) $button->attr('class');
+
+        self::assertStringContainsString('ids-expander', $classAttr, 'Base class should be present');
+        self::assertStringContainsString('u-mt-2', $classAttr, 'Custom class u-mt-2 should be merged');
+        self::assertStringContainsString('custom-hook', $classAttr, 'Custom class custom-hook should be merged');
+    }
+
+    private function getButton(Crawler $crawler): Crawler
+    {
+        return $crawler->filter('button.ids-expander');
+    }
+}
---
+++ tests/integration/Twig/Components/HelperTextTest.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\Integration\DesignSystemTwig\Twig\Components;
+
+use Generator;
+use Ibexa\DesignSystemTwig\Twig\Components\HelperText;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\UX\TwigComponent\Test\InteractsWithTwigComponents;
+
+final class HelperTextTest extends KernelTestCase
+{
+    use InteractsWithTwigComponents;
+
+    public function testMount(): void
+    {
+        $component = $this->mountTwigComponent(
+            'ibexa:HelperText',
+            [
+                'type' => 'error',
+            ]
+        );
+
+        self::assertInstanceOf(HelperText::class, $component, 'Component should be instance of HelperText');
+        self::assertSame('error', $component->type, 'Type should be "error"');
+        self::assertSame('alert-error', $component->iconName(), 'Icon name should be passed through');
+    }
+
+    public function testDefaultRender(): void
+    {
+        $rendered = $this->renderTwigComponent('ibexa:HelperText');
+        $crawler = $rendered->crawler();
+
+        $container = $this->getHelperText($crawler);
+
+        self::assertSame(1, $container->count(), 'There should be exactly one helper text container');
+        $classAttr = (string) $container->attr('class');
+
+        self::assertStringContainsString('ids-helper-text', $classAttr, 'Base helper-text class should be present');
+        self::assertStringContainsString('ids-helper-text--default', $classAttr, 'Default variant should be "default"');
+
+        self::assertSame(1, $container->filter('.ids-helper-text__icon-wrapper')->count(), 'Icon wrapper should be rendered');
+        self::assertSame(1, $container->filter('.ids-helper-text__content-wrapper')->count(), 'Content wrapper should be rendered');
+    }
+
+    public function testErrorVariantAddsErrorClass(): void
+    {
+        $rendered = $this->renderTwigComponent('ibexa:HelperText', [
+            'type' => 'error',
+        ]);
+        $crawler = $rendered->crawler();
+
+        $container = $this->getHelperText($crawler);
+        $classAttr = (string) $container->attr('class');
+
+        self::assertStringContainsString('ids-helper-text--error', $classAttr, 'Error variant should add error modifier class');
+        self::assertStringNotContainsString('ids-helper-text--default', $classAttr, 'Default class should not be present when type=error');
+    }
+
+    public function testMergesCustomClassesFromAttributes(): void
+    {
+        $rendered = $this->renderTwigComponent('ibexa:HelperText', [
+            'attributes' => [
+                'class' => 'u-mb-2 custom-class',
+                'data-test' => 'helper-text-x',
+            ],
+        ]);
+        $crawler = $rendered->crawler();
+
+        $container = $this->getHelperText($crawler);
+        $classAttr = (string) $container->attr('class');
+
+        self::assertStringContainsString('ids-helper-text', $classAttr, 'Base class should be present');
+        self::assertStringContainsString('u-mb-2', $classAttr, 'Custom class u-mb-2 should be merged');
+        self::assertStringContainsString('custom-class', $classAttr, 'Custom class custom-class should be merged');
+        self::assertSame('helper-text-x', $container->attr('data-test'), 'Custom attribute should be preserved');
+    }
+
+    /**
+     * @dataProvider iconByTypeProvider
+     *
+     * @param array<string,mixed> $props
+     */
+    public function testIconIsRenderedForType(array $props, string $expectedIconId): void
+    {
+        $rendered = $this->renderTwigComponent('ibexa:HelperText', $props);
+
+        $iconUse = $rendered->crawler()->filter('.ids-helper-text__icon use')->first();
+        self::assertSame(1, $iconUse->count(), 'Icon <use> element should be rendered');
+
+        $href = $iconUse->attr('xlink:href');
+        self::assertNotEmpty($href, 'Icon href should not be empty');
+        self::assertStringEndsWith(
+            $expectedIconId,
+            $href,
+            sprintf('Icon href should end with "%s" for props: %s', $expectedIconId, json_encode($props))
+        );
+    }
+
+    /**
+     * @return Generator<string, array{0: array<string, mixed>, 1: string}>
+     */
+    public static function iconByTypeProvider(): Generator
+    {
+        yield 'default type -> info icon' => [
+            [],
+            '#info-circle',
+        ];
+
+        yield 'error type -> alert icon' => [
+            ['type' => 'error'],
+            '#alert-error',
+        ];
+    }
+
+    private function getHelperText(Crawler $crawler): Crawler
+    {
+        return $crawler->filter('div.ids-helper-text');
+    }
+}
