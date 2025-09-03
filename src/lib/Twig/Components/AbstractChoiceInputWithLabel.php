39424ce501de368f77ba9225356f4b5416994be6
Michał Grabowski <michal.grabowski@ibexa.co>
2025-09-03 14:29:01 +0200

    IBX-10606: Base ChoiceInput with Label

---
+++ src/lib/Twig/Components/AbstractChoiceInputWithLabel.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\DesignSystemTwig\Twig\Components;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\UX\TwigComponent\Attribute\PreMount;
+
+abstract class AbstractChoiceInputWithLabel
+{
+    public string $id;
+
+    /**
+     * @param array<string, mixed> $props
+     *
+     * @return array<string, mixed>
+     */
+    #[PreMount]
+    public function validate(array $props): array
+    {
+        $resolver = new OptionsResolver();
+        $resolver->setIgnoreUndefined();
+        $resolver
+            ->define('for')
+            ->required()
+            ->allowedTypes('string');
+
+        $this->configurePropsResolver($resolver);
+
+        return $resolver->resolve($props) + $props;
+    }
+
+    abstract protected function configurePropsResolver(OptionsResolver $resolver): void;
+}
