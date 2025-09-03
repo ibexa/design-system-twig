68801718287725391b60e6c16fd95d98dc18eba0
Michał Grabowski <michal.grabowski@ibexa.co>
2025-09-03 15:34:55 +0200

    rename with label to field

--- src/lib/Twig/Components/AbstractChoiceInputField.php
+++ src/lib/Twig/Components/AbstractChoiceInputField.php
@@ -11,7 +11,7 @@ namespace Ibexa\DesignSystemTwig\Twig\Components;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\UX\TwigComponent\Attribute\PreMount;
 
-abstract class AbstractChoiceInputWithLabel
+abstract class AbstractChoiceInputField
 {
     public string $id;
 
