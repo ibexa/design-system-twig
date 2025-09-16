d8aed30bcd70eb21fe1002d316428437c1679cea
Michał Grabowski <michal.grabowski@ibexa.co>
2025-09-16 16:28:05 +0200

    IBX-10667: Merge ThreeState and Checkbox components

--- src/bundle/Resources/public/ts/components/checkbox/checkbox_input.ts
+++ src/bundle/Resources/public/ts/components/checkbox/checkbox_input.ts
@@ -1,3 +1,21 @@
 import { BaseChoiceInput } from '../../partials';
 
-export class CheckboxInput extends BaseChoiceInput {}
+export class CheckboxInput extends BaseChoiceInput {
+    constructor(container: HTMLDivElement) {
+        super(container);
+
+        this.setIndeterminate(this._inputElement.classList.contains('ids-input--indeterminate'));
+    }
+
+    setIndeterminate(value: boolean) {
+        this._inputElement.indeterminate = value;
+        this._inputElement.classList.toggle('ids-input--indeterminate', value);
+
+        if (value) {
+            this._inputElement.checked = false;
+
+            this._inputElement.dispatchEvent(new Event('input', { bubbles: true }));
+            this._inputElement.dispatchEvent(new Event('change', { bubbles: true }));
+        }
+    }
+}
--- src/bundle/Resources/public/ts/components/checkbox/index.ts
+++ src/bundle/Resources/public/ts/components/checkbox/index.ts
@@ -1,2 +1,1 @@
 export * from './checkbox_input';
-export * from './three_state_checkbox_input';
