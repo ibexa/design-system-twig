20d3118a74ae1795d4327d97e1871870904fa531
Aleksandra Bożek <aleksandra.m.bozek@gmail.com>
2025-09-16 14:33:05 +0200

    IBX-7880: Badge (#14)
    Co-authored-by: mikolaj <mikolaj.adamczyk@ez.no>

---
+++ src/lib/Twig/Components/Badge.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\DesignSystemTwig\Twig\Components;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use Symfony\UX\TwigComponent\Attribute\ExposeInTemplate;
+use Symfony\UX\TwigComponent\Attribute\PreMount;
+
+#[AsTwigComponent('ibexa:badge')]
+final class Badge
+{
+    private const DEFAULT_MAX_BADGE_VALUE = 99;
+    private const THRESHOLD = [
+        'medium' => 100,
+        'small' => 10,
+    ];
+
+    public string $size = 'medium';
+
+    public int $value = 1;
+
+    #[ExposeInTemplate('max_value')]
+    public int $maxBadgeValue;
+
+    public function __construct()
+    {
+        $this->maxBadgeValue = self::DEFAULT_MAX_BADGE_VALUE;
+    }
+
+    /**
+     * @param array<string, mixed> $props
+     *
+     * @return array<string, mixed>
+     */
+    #[PreMount]
+    public function validate(array $props): array
+    {
+        $resolver = new OptionsResolver();
+        $resolver->setIgnoreUndefined(true);
+        $resolver
+            ->define('size')
+            ->allowedValues('small', 'medium')
+            ->default('medium');
+        $resolver
+            ->define('value')
+            ->allowedTypes('int')
+            ->required();
+        $resolver
+            ->define('maxBadgeValue')
+            ->allowedTypes('int')
+            ->default(self::DEFAULT_MAX_BADGE_VALUE);
+
+        return $resolver->resolve($props);
+    }
+
+    #[ExposeInTemplate('is_stretched')]
+    public function isStretched(): bool
+    {
+        return $this->value >= self::THRESHOLD[$this->size];
+    }
+
+    #[ExposeInTemplate('formatted_value')]
+    public function getFormattedValue(): string
+    {
+        if ($this->value > $this->maxBadgeValue) {
+            return $this->maxBadgeValue . '+';
+        }
+
+        return (string)$this->value;
+    }
+}
