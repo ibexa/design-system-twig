103ca6d1161fdb3060ff938850115b5edbfcc6fb
Michał Grabowski <michal.grabowski@ibexa.co>
2025-09-23 17:22:58 +0200

    IBX-10619: RadioButtonsListField

--- src/bundle/Resources/public/ts/components/radio_button/index.ts
+++ src/bundle/Resources/public/ts/components/radio_button/index.ts
@@ -1,1 +1,2 @@
 export * from './radio_button_input';
+export * from './radio_buttons_list_field';
--- src/bundle/Resources/public/ts/partials/index.ts
+++ src/bundle/Resources/public/ts/partials/index.ts
@@ -1,3 +1,4 @@
 export * from './base';
 export * from './base_choice_input';
 export * from './base_field';
+export * from './base_inputs_list';
