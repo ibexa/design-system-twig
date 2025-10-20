a8dc475942303617abba54a4ef8c6acd2ff1f2b3
mikolaj <mikolaj.adamczyk@ez.no>
2025-10-20 11:18:29 +0200

    Code improvements

--- src/lib/Twig/Components/OverflowList.php
+++ src/lib/Twig/Components/OverflowList.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 
 namespace Ibexa\DesignSystemTwig\Twig\Components;
 
+use InvalidArgumentException;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 use Symfony\UX\TwigComponent\Attribute\ExposeInTemplate;
@@ -39,7 +40,28 @@ final class OverflowList
         $resolver
             ->define('items')
             ->allowedTypes('array')
-            ->default([]);
+            ->default([])
+            ->normalize(static function ($options, $value): array {
+                if (!is_array($value) || !array_is_list($value)) {
+                    throw new InvalidArgumentException('Property "items" must be a list (sequential array).');
+                }
+
+                foreach ($value as $i => $item) {
+                    if (!is_array($item)) {
+                        throw new InvalidArgumentException(sprintf('items[%d] must be an array, %s given.', $i, get_debug_type($item)));
+                    }
+                    foreach (array_keys($item) as $key) {
+                        if (!is_string($key)) {
+                            throw new InvalidArgumentException(sprintf('items[%d] must use string keys.', $i));
+                        }
+                        if (!preg_match('/^[A-Za-z_][A-Za-z0-9_]*$/', $key)) {
+                            throw new InvalidArgumentException(sprintf('Invalid key "%s" in items[%d].', $key, $i));
+                        }
+                    }
+                }
+
+                return $value;
+            });
         $resolver
             ->define('itemTemplateProps')
             ->allowedTypes('array')
@@ -54,17 +76,18 @@ final class OverflowList
     #[ExposeInTemplate('item_template_props')]
     public function getItemTemplateProps(): array
     {
-        $itemPropsNames = array_keys($this->items[0]);
-
-        $itemPropsPatterns = array_map(
-            static fn (string $name): string => '{{ ' . $name . ' }}',
-            $this->itemTemplateProps
-        );
-
-        $props = array_combine($itemPropsNames, $itemPropsPatterns);
-
-        if (empty($props)) {
+        if (empty($this->items)) {
             return [];
+        }
+
+        $allKeys = [];
+        foreach ($this->items as $item) {
+            $allKeys = array_unique([...$allKeys, ...array_keys($item)]);
+        }
+
+        $props = [];
+        foreach ($allKeys as $name) {
+            $props[$name] = '{{ ' . $name . ' }}';
         }
 
         return $props;
