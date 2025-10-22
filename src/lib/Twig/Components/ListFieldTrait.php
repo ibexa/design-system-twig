5121eeac1af5303adb0ceb8ad5770fa9e6c88eb0
mikolaj <mikolaj.adamczyk@ez.no>
2025-10-22 14:14:38 +0000

    Added validation for 'items' option in ListFieldTrait

--- src/lib/Twig/Components/ListFieldTrait.php
+++ src/lib/Twig/Components/ListFieldTrait.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 
 namespace Ibexa\DesignSystemTwig\Twig\Components;
 
+use Symfony\Component\OptionsResolver\Exception\InvalidOptionsException;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\UX\TwigComponent\Attribute\ExposeInTemplate;
 
@@ -57,6 +58,41 @@ trait ListFieldTrait
             'items' => [],
         ]);
         $resolver->setAllowedTypes('items', 'array');
+        $resolver->setAllowedValues('items', static function (array $items): bool {
+            foreach ($items as $index => $item) {
+                if (!is_array($item)) {
+                    throw new InvalidOptionsException(
+                        sprintf('Item at index %d must be an array, %s given.', $index, get_debug_type($item))
+                    );
+                }
+
+                if (!array_key_exists('value', $item)) {
+                    throw new InvalidOptionsException(
+                        sprintf('Item at index %d must define a "value" key.', $index)
+                    );
+                }
+
+                if (!array_key_exists('label', $item)) {
+                    throw new InvalidOptionsException(
+                        sprintf('Item at index %d must define a "label" key.', $index)
+                    );
+                }
+
+                if (!is_string($item['label'])) {
+                    throw new InvalidOptionsException(
+                        sprintf('Item at index %d must define a "label" string, %s given.', $index, get_debug_type($item['label']))
+                    );
+                }
+
+                if (!is_string($item['value']) && !is_int($item['value'])) {
+                    throw new InvalidOptionsException(
+                        sprintf('Item at index %d must define a "value" as string or int, %s given.', $index, get_debug_type($item['value']))
+                    );
+                }
+            }
+
+            return true;
+        });
 
         $resolver
             ->define('direction')
