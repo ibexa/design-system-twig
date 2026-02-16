bece9c62527c8544f629ff41560ae7f73b0f4690
tischsoic <symfiz@gmail.com>
2026-02-16 07:37:13 +0100

    IBX-11332: Add Link component

---
+++ src/lib/Twig/Components/Link.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\DesignSystemTwig\Twig\Components;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use Symfony\UX\TwigComponent\Attribute\ExposeInTemplate;
+use Symfony\UX\TwigComponent\Attribute\PreMount;
+
+#[AsTwigComponent('ibexa:link')]
+final class Link
+{
+    public string $href;
+
+    public string $variant = 'button';
+
+    public string $size = 'medium';
+
+    public string $type = 'tertiary';
+
+    public string $icon = '';
+
+    /**
+     * @var array{small: string, medium: string}
+     */
+    private static array $iconSizeMap = [
+        'small' => 'tiny-small',
+        'medium' => 'small',
+    ];
+
+    /**
+     * @param array<string, mixed> $props
+     *
+     * @return array<string, mixed>
+     */
+    #[PreMount]
+    public function validate(array $props): array
+    {
+        $resolver = new OptionsResolver();
+        $resolver->setIgnoreUndefined();
+        $resolver
+            ->define('href')
+            ->required()
+            ->allowedTypes('string');
+        $resolver
+            ->define('variant')
+            ->allowedValues('button', 'text')
+            ->default('button');
+        $resolver
+            ->define('size')
+            ->allowedValues('small', 'medium')
+            ->default('medium');
+        $resolver
+            ->define('type')
+            ->allowedValues('primary', 'secondary', 'tertiary', 'secondary-alt', 'tertiary-alt')
+            ->default('tertiary');
+        $resolver
+            ->define('icon')
+            ->allowedTypes('string');
+
+        return $resolver->resolve($props) + $props;
+    }
+
+    #[ExposeInTemplate('icon_size')]
+    public function iconSize(): string
+    {
+        return self::$iconSizeMap[$this->size];
+    }
+}
